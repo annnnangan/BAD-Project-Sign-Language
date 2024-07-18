10c58f7248034cbf789a3624a64cb15cfb21580c
annnnangan <anna.ymngan@gmail.com>
2024-07-18 18:07:37 +0800

    update html,js of which the learning modal ui logic

--- api-server/protected/games/games.js
+++ api-server/protected/games/games.js
@@ -61,20 +61,9 @@ async function loadPage() {
     const currentLearningAlphabet = event.target.getAttribute("data-alphabet");
 
     // send image to server
-    const response = await fetch("/upload", {
-      method: "POST",
-      cache: "no-cache",
-      credentials: "same-origin",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        imageString: imageString,
-      }),
-    });
 
-    const detectedCharacter = (await response.json()).data;
-
+    appendModal();
+    const feedbackModal = document.querySelector("#feedbackModal");
     const feedback = document.querySelector(".feedback");
     const characterFace = document.querySelector(".character-face");
     const modalTitle = document.querySelector(".feedback .modal-title");
@@ -82,8 +71,60 @@ async function loadPage() {
       ".feedback .modal-description"
     );
     const backButton = document.querySelector(".feedback .modal-footer button");
+    try {
+      const response = await fetch("/upload", {
+        method: "POST",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          imageString: imageString,
+        }),
+      });
+      const detectedCharacter = (await response.json()).data;
 
-    if (currentLearningAlphabet != detectedCharacter.toLowerCase()) {
+      console.log(
+        "compare result",
+        currentLearningAlphabet != detectedCharacter.toLowerCase()
+      );
+
+      if (currentLearningAlphabet != detectedCharacter.toLowerCase()) {
+        throw Error();
+      } else {
+        await fetch(`/games/complete-list/${currentLearningAlphabet}`, {
+          method: "PUT",
+        });
+        characterFace.src = `../assets/character/monster-excited-face.png`;
+        modalTitle.innerText = "Congratulations!";
+        modalDescription.innerText = `You've got it right!`;
+        backButton.innerText = `Back`;
+        backButton.setAttribute("data-bs-dismiss", "modal");
+        backButton.removeAttribute("data-bs-target");
+        backButton.removeAttribute("data-bs-toggle");
+
+        feedbackModal.classList.toggle("d-block");
+
+        backButton.addEventListener("click", (e) => {
+          e.preventDefault();
+          feedbackModal.classList.toggle("d-block");
+        });
+        confetti({
+          particleCount: 500,
+          spread: 120,
+          origin: { x: 1, y: 0.9 },
+        });
+
+        confetti({
+          particleCount: 500,
+          spread: 120,
+          origin: { x: 0, y: 0.9 },
+        });
+
+        loadAlphabet();
+      }
+    } catch (error) {
       characterFace.src = `../assets/character/monster-sad-face.png`;
       modalTitle.innerText = "It seems wrong.";
       modalDescription.innerText = `Never Give Up and Try Again!`;
@@ -91,30 +132,70 @@ async function loadPage() {
       backButton.removeAttribute("data-bs-dismiss");
       backButton.setAttribute("data-bs-target", "#learningModal");
       backButton.setAttribute("data-bs-toggle", "modal");
-    } else {
-      await fetch(`/games/complete-list/${currentLearningAlphabet}`, {
-        method: "PUT",
-      });
-      characterFace.src = `../assets/character/monster-excited-face.png`;
-      modalTitle.innerText = "Congratulations!";
-      modalDescription.innerText = `You've got it right!`;
-      backButton.innerText = `Back`;
-      backButton.setAttribute("data-bs-dismiss", "modal");
-      backButton.removeAttribute("data-bs-target");
-      backButton.removeAttribute("data-bs-toggle");
-      confetti({
-        particleCount: 500,
-        spread: 120,
-        origin: { x: 1, y: 0.9 },
-      });
 
-      confetti({
-        particleCount: 500,
-        spread: 120,
-        origin: { x: 0, y: 0.9 },
+      backButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        feedbackModal.classList.toggle("d-block");
+        startWebCam();
       });
-
-      loadAlphabet();
+      feedbackModal.classList.toggle("d-block");
     }
   });
+}
+
+function appendModal() {
+  let target = document.querySelector("#feedback-modal-container");
+
+  target.innerHTML = `<div
+      class="modal"
+      id="feedbackModal"
+      aria-hidden="true"
+      aria-labelledby="feedbackModalLabel"
+      tabindex="-1"
+    >
+      <div class="modal-dialog modal-dialog-centered feedback">
+        <div class="modal-content">
+          <div class="modal-header">
+            <img
+              class="character-face"
+              src="../assets/character/monster-excited-face.png"
+              alt=""
+            />
+
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <h5 class="modal-title">Congratulations!</h5>
+            <p class="modal-description">You've got it right!</p>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Back
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+`;
+}
+
+async function startWebCam() {
+  try {
+    const video = document.querySelector(".web-cam");
+    console.log("hello");
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    video.srcObject = stream;
+    window.stream = stream;
+  } catch (e) {
+    console.log(e.toString());
+  }
 }
